999752235d66141e2ca72b2ca3c8fef60e48f7f7
Timur Alikhanov <55821089+VictoriaAutMors@users.noreply.github.com>
2019-10-08 15:43:21 +0600

    Add files via upload

---
+++ hw2/8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    char a[4];
+    char ch;
+    int count;
+    do {
+        count = 0;
+        do {
+            ch = getchar();
+            a[count] = ch;
+            count++;
+        } while(count != 4 && ch != ' ' && ch != '\n');
+        if (ch != ' ') {
+            while(ch != ' ' && ch != '\n') {
+                ch = getchar();
+            }
+        } else if (count == 4) {
+            for (count = 0; count < 4; count++) {
+                putchar(a[count]);
+            }
+        }
+    } while(ch != '\n');
+    putchar('\n');
+    return 0;
+}
